ef93860ca586b075d952a7d3f56079d5d2fce0b2
Osher728 <osherosher728@gmail.com>
2022-08-27 17:35:15 -0500

    [Proyecto Base - Curso Vaadin Osher 2022]

---
+++ src/main/java/com/osher/cursovaadin/OsherVaadin2022Application.java
@@ -0,0 +1,13 @@
+package com.osher.cursovaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsherVaadin2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsherVaadin2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/osher/cursovaadin/OsherVaadin2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osher.cursovaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsherVaadin2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
